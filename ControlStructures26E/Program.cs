0c4cb661ef813b2e1aac381c44ce6702fe28c0e5
Poul Henriksen <pohe@zealand.dk>
2026-09-17 09:20:41 +0200

    Add project files.

---
+++ ControlStructures26E/Program.cs
@@ -0,0 +1,51 @@
+﻿Console.WriteLine("Hello, World!");
+
+Console.Write("Indtast antal børn:");
+int noOfChildren = int.Parse(Console.ReadLine());
+int childSupport = 0;
+switch(noOfChildren)
+{
+    case 0:
+        childSupport = 0;
+        break;
+    case 1:
+        childSupport = 1200;
+        break;
+    case 2:
+        childSupport = 2000;
+        break;
+    case 3:
+        childSupport = 2600;
+        break;
+    default:
+        childSupport = 3000;
+        break;
+    }
+
+Console.WriteLine(childSupport);
+
+int score = 66;
+string mark = ""; 
+switch (score)
+{
+    case (< 40):
+        mark = "D";
+        break;
+    case (< 70):
+        mark = "C";
+        break;
+    case (< 90):
+        mark = "B";
+        break;
+    default:
+        mark = "A";
+        break;
+}
+
+
+int number = 1;
+while (number < 5)
+{
+    Console.WriteLine($"The value of the number is {number}");
+    number = number + 2;
+}
